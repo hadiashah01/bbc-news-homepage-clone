acba6f90fc91091fa5c9f7968ffa1cb795780a59
Hadia <hadiashah010@gmail.com>
2026-06-10 00:32:30 +0500

    select h1.title and replace its textContent with newsData.articles[i].title using for loop

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
+let headline = document.querySelectorAll(".title");
+let fetchNews= async () => {
+  let data = await fetch(
+    "https://gnews.io/api/v4/top-headlines?category=general&lang=en&country=us&max=10&apikey=d9b390844e791a0e5f31195889a6b00f",
+  );
+  let newsData = await data.json();
+  console.log(newsData);
+  for (let i = 0; i < headline.length; i++) {
+    console.log(ntextContentewsData);
+    headline[i]. = newsData.articles[i].title;
+  }
+};
+fetchNews();
 
-let  variable= async () => {
-    let data = await fetch("https://gnews.io/api/v4/top-headlines?category=general&lang=en&country=us&max=10&apikey=27e2b5eb77871edbb524e632963bb0ca")
-    let newsData = await data.json()
-    console.log(newsData )
-}
-variable();
