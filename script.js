e9cf66aebd334bebe1026186a73c519dfc80b5eb
Hadia <hadiashah010@gmail.com>
2026-06-10 17:31:33 +0500

    format js file and add event listeners using showLanguages() & hideLanguages() on .languages__toggle selected as button

--- script.js
+++ script.js
@@ -2,20 +2,34 @@ let headline = document.querySelectorAll(".title");
 let description = document.querySelectorAll(".description");
 let img = document.querySelectorAll(".news-img");
 
-let fetchNews= async () => {
+let fetchNews = async () => {
   let data = await fetch(
     "https://gnews.io/api/v4/top-headlines?category=general&lang=en&country=us&max=10&apikey=d9b390844e791a0e5f31195889a6b00f",
   );
   let newsData = await data.json();
   console.log(newsData);
   for (let i = 0; i < headline.length; i++) {
-      headline[i].textContent = newsData.articles[i].title;
-      description[i].textContent = newsData.articles[i].description;
-      img[i].innerHTML=`<img
+    headline[i].textContent = newsData.articles[i].title;
+    description[i].textContent = newsData.articles[i].description;
+    img[i].innerHTML = `<img
                 src="${newsData.articles[i].image}"
                 alt=""
-              />`
-    }
+              />`;
+  }
 };
 fetchNews();
 
+let languages = document.querySelector(".languages");
+function hideLanguages() {
+  console.log("btn is clicked");
+  languages.style.display = "none";
+}
+
+let button = document.querySelector(".languages__toggle");
+button.addEventListener("click", hideLanguages);
+function showLanguages() {
+  console.log("NOW btn is clicked twice");
+  languages.style.display = "flex";
+}
+
+button.addEventListener("dblclick", showLanguages);
